d390d624c66e97e0b1f5b867c2cca685c58bf7b8
Jose Mejia <josemejia247@gmail.com>
2021-01-07 15:30:20 -0500

    finished phase 2 with code review

---
+++ todo/frontend/reducers/todos_reducer.js
@@ -0,0 +1,38 @@
+import  {RECEIVE_TODOS, RECEIVE_TODO} from '../actions/todo_actions';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false,
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true,
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  debugger
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_TODOS:
+      action.todos.forEach(el => {
+        newState[el.id] = el;
+      })
+      return newState;
+    case RECEIVE_TODO:
+      newState[action.todo.id] = action.todo;
+      return newState; 
+    default:
+      return state;
+  }
+};
+
+
+
+export default todosReducer;
--- todo/frontend/todo_redux.jsx
+++ todo/frontend/todo_redux.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import configureStore from './store/store.js'
+import { receiveTodos, receiveTodo } from './actions/todo_actions.js'
+
+const store = configureStore();
+window.store = store;
+window.receiveTodos = receiveTodos;
+window.receiveTodo = receiveTodo;
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root')
     ReactDOM.render(<h1>Todos App</h1>, root);
